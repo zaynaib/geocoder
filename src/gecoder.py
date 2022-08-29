5850a52b073bb65a5ada15a2130b69201f9f69b2
zaynaib <zaynaibg@gmail.com>
2022-08-29 16:51:57 -0500

    read in cook county locations and get rid of null post codes

--- src/gecoder.py
+++ src/gecoder.py
@@ -1,66 +1,22 @@
 import pandas as pd
-import sqlite3
-import json, csv
-import re
-import numpy as np
-import time
 
-
-######
-#read in cook county address
+###### read in cook county address ######
 address_points = pd.read_csv('cook_locations.csv')
+
+#print(address_points.head())
+
 cook_locations = address_points[['Add_Number', 'ADDRDELIV','CMPADDABRV','Lat', 'Long','PLACENAME','Post_Code']]
+#print(cook_locations.head)
+#print(cook_locations.shape)
+
+#get rid of null zip code values
 cook_locations_clean = cook_locations[~cook_locations['Post_Code'].isnull()]
-
-
-#read oemc locations
-omec_locations = pd.read_csv('oemc_locations.csv')
-omec_locations.head() 
-
-#data cleaning convert numerical into integer so there won't be in discrepancy 
-omec_clean_locations = omec_locations[~omec_locations['numerical'].str.isupper()]
-omec_clean_locations = omec_clean_locations[omec_clean_locations['EventNumber'] != 2011910398 ]
-omec_clean_locations['numerical'] = omec_clean_locations['numerical'].astype(str)
-omec_clean_locations['numerical'] = omec_clean_locations['numerical'].str.strip()
-omec_clean_locations['numerical'] = omec_clean_locations['numerical'].astype(int)
-
-
-#copy original street name for clean up
-omec_clean_locations['clean_street_name'] = omec_clean_locations['street_name']
-
-
-omec_s = omec_clean_locations[omec_clean_locations['street_name'].str.contains("/",na=False)]
-print(omec_s.head())
-
-#clean up street names with slashes but removing everything after the slash in location
-omec_s['clean_street_name'] = omec_s['street_name'].str.replace("(?:/).*",'',regex=True)
-
-
-#####
-
-###
-#### HELPER FUNCTIONS
-
-#create function to split upt clean_street_name and then just grab the street name
-def splitUp(word):
-    w = word.split(' ')
-    return w[0]
-    
-
-def fullDirections(direction):
-    if direction == 'W':
-        return 'WEST'
-    if direction == 'E':
-        return 'EAST'
-    if direction == 'S':
-        return 'SOUTH'
-    if direction == 'N':
-        return 'NORTH'
-    else:
-        return direction
-    
+#print(cook_locations_clean.shape)
 
 
 
+###### read in oemc data ######
 
+oemc_locations = pd.read_csv('oemc_locations.csv')
+print(oemc_locations.head())
 
