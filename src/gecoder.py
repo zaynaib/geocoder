46cb88b619d50086f49b020d2c75187ff7d4c2ab
zaynaib <zaynaibg@gmail.com>
2022-08-29 17:16:29 -0500

    abstract columns of cook_locations

--- src/gecoder.py
+++ src/gecoder.py
@@ -1,11 +1,35 @@
 import pandas as pd
 
+
+##### Helper functions ######
+
+#create function to split upt clean_street_name and then just grab the street name
+def splitUp(word):
+    w = word.split(' ')
+    return w[0]
+    
+def fullDirections(direction):
+    if direction == 'W':
+        return 'WEST'
+    if direction == 'E':
+        return 'EAST'
+    if direction == 'S':
+        return 'SOUTH'
+    if direction == 'N':
+        return 'NORTH'
+    else:
+        return direction
+
+
+
 ###### read in cook county address ######
 address_points = pd.read_csv('cook_locations.csv')
 
 #print(address_points.head())
 
-cook_locations = address_points[['Add_Number', 'ADDRDELIV','CMPADDABRV','Lat', 'Long','PLACENAME','Post_Code']]
+cook_columns = ['CMPADDABRV', 'Lat', 'Long', 'PLACENAME', 'Post_Code', 'OBJECTID','ADDRDELIV','Add_Number','St_PreDir', 'St_Name']
+
+cook_locations = address_points[cook_columns]
 #print(cook_locations.head)
 #print(cook_locations.shape)
 
@@ -15,7 +39,6 @@ cook_locations_clean = cook_locations[~cook_locations['Post_Code'].isnull()]
 
 
 
-###### read in oemc data ######
 
 oemc_locations = pd.read_csv('oemc_locations.csv')
 oemc_clean_locations = oemc_locations[~oemc_locations['numerical'].str.isupper()]
@@ -40,9 +63,36 @@ oemc_clean_locations['numerical'] = oemc_clean_locations['numerical'].astype(int
 #print(oemc_clean_locations.dtypes)
 
 
-### Clean up street names in oemc data
+### Clean up street names in oemc data #####
 
 oemc_clean_locations['clean_street_name'] = oemc_clean_locations['street_name']
+
+
+omec_nonS = oemc_clean_locations[~oemc_clean_locations['street_name'].str.contains("/",na=False)]
+omec_s = oemc_clean_locations[oemc_clean_locations['street_name'].str.contains("/",na=False)]
+
+#grab the beginning of the string until the forward slash /
+omec_s['clean_street_name'] = omec_s['street_name'].str.replace("(?:/).*",'',regex=True)
+omec_s['clean_street_name'] = omec_s['clean_street_name'].apply(splitUp)
+omec_complete = pd.concat([omec_nonS,omec_s])
+
+ 
+#print(omec_nonS.shape)
+#print(omec_s.shape)
+#print(oemc_clean_locations.head())
+#print(omec_s.head())
+#print(omec_complete)
+
+###### clean oemc data directional ######
+
+#strip the any trailing white space
+omec_complete['directional'] = omec_complete['directional'].str.strip()
+omec_complete['directional']= omec_complete['directional'].apply(fullDirections)
+
+
+
+
+
 
 
 
